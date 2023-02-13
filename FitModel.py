c262c62557b27a25ddcfe1c3413dbc9aeb1c6439
Troy Loeffler <troydloeffler@gmail.com>
2023-02-13 14:25:09 -0800

    Finishing training code

--- FitModel.py
+++ FitModel.py
@@ -1,5 +1,15 @@
 import numpy as np
 from LorentzModel import CorrLengthModel
+import jax
+import jax.numpy as jnp
+from jax import grad, jit
+
 
 model = CorrLengthModel(temperature=350.0)
-data = np.load_data("data.txt")
+data = np.loadtxt("radialplot_Avg_xy.dump")
+print(data)
+print(data.shape)
+Q, Y = data[:,0], data[:,1]
+
+
+model.fit(Q,Y, lrate=1e-7)
--- LorentzModel.py
+++ LorentzModel.py
@@ -1,36 +1,64 @@
-import numpy as np
+#import numpy as np
+import jax.numpy as np
+import jax
+import optax
 
+def rmse(Y_model, Y_target):
+    err = Y_model - Y_target
+    err = np.square(err)
+    err = np.mean(err)
+    return err
 #A function which takes a set of X and Y data and fits a linear regression model to it.
 class CorrLengthModel:
     def __init__(self, temperature=350.0):
         self.w_n = 0.0
         self.I0 = 1.0
         self.e0 = 1.0
-        self.Tsp = 1.0
+        self.Tsp = temperature+100.0
         self.temp = temperature
         self.gamma = 1.0
         self.nu = 1.0
+        self.parameters = [self.w_n, self.Tsp, self.gamma, self.nu, self.I0, self.e0]
+        self.parameters = np.array(self.parameters)
+
+    def fit(self, Q, Y, lrate = 1e-6):
+        optimizer = optax.adam(lrate)
+        opt_state = optimizer.init(self.parameters)
+        for iepoch in range(1000):
+            err, gradient = jax.value_and_grad(self.computeerror)(Q, Y)
+            updates, opt_state = optimizer.update(gradient, opt_state)
+            curpos = optax.apply_updates(curpos, updates)
+            if iepoch%10 == 0:
+                print("Epoch: %s, Training Error: %s"%(iepoch, err))
+
+    def computeerror(self, Q, Y):
+        Y_model = self.predict(Q)
+        err = rmse(Y, Y_model)
+        print(err)
+        return err
 
     def predict(self, Q):
-        #Fit the model to the data
-        X = Q * self.e0_func
+        X = Q * self.e0_func()
         a = self.Iq0_func()
         Y_model = self.lorentzian(X, a)
         return Y_model
 
     def lorentzian(self, X, a):
-        return a / ( 1 + np.power(X,2-self.w_n) )
+#        return a / ( 1 + np.power(X,2-self.w_n) )
+        return a / ( 1 + np.power(X,2-self.parameters[0]) )
 
     def Iq0_func(self):
-        Iq0 = self.I0 * np.power( (self.temp-self.Tsp)/(self.Tsp), -self.gamma) )
+#        Iq0 = self.I0 * np.power( (self.temp-self.Tsp)/(self.Tsp), -self.gamma) )
+        Iq0 = self.parameters[4] * np.power( (self.temp-self.parameters[1])/(self.parameters[1]), -self.parameters[2]) 
         return Iq0
 
     def e0_func(self):
-        eq0 = self.e0 * np.power( (self.temp-self.Tsp)/(self.Tsp), -self.nu) )
+        eq0 = self.parameters[5] * np.power( (self.temp-self.parameters[1])/(self.parameters[1]), -self.parameters[3]) 
         return eq0
 
     def get_weights(self):
-        return [self.w_n, self.Tsp, self.gamma, self.nu, self.I0, self.e0]
+#        return [self.w_n, self.Tsp, self.gamma, self.nu, self.I0, self.e0]
+        return self.parameters
 
     def set_weights(self, weights):
         self.w_n, self.Tsp, self.gamma, self.nu, self.I0, self.e0 = tuple(weights)
